d209a7cdc2e0191a8f36b529b8e2378402efd09d
mc <162787712+mc-str@users.noreply.github.com>
2026-08-30 09:21:15 +0800

    Add files via upload

---
+++ data.js
@@ -0,0 +1,135 @@
+window.siteData = {
+  "downloadConfig": {
+    "mode": "auto",
+    "fallbackToDirect": true,
+    "showSpeed": true
+  },
+  "products": [
+    {
+      "name": "雪球T显生成器",
+      "desc": "编辑后可一键生成静态雪球菜单显示部分。",
+      "icon": "https://s41.ax1x.com/2026/03/20/penMoUx.png",
+      "badge": "official",
+      "badgeText": "正式版",
+      "download": {
+        "enabled": true,
+        "url": "https://lz.qaiu.top/parser?url=https://wwp.lanzoup.com/iPZ283gfknib&pwd=aqgi",
+        "platform": "Android",
+        "version": "v1.5",
+        "size": "6.02 MB",
+        "mode": "auto",
+        "filename": "雪球T显生成器_1.5.apk"
+      }
+    },
+    {
+      "name": "动态菜单生成器",
+      "desc": "一站式设计快捷编辑,自动生成全部动态雪球菜单指令",
+      "icon": "https://s41.ax1x.com/2026/03/20/penl3ff.png",
+      "badge": "official",
+      "badgeText": "正式版",
+      "download": {
+        "enabled": true,
+        "url": "https://lz.qaiu.top/parser?url=https://wwp.lanzoup.com/iMDSY3h1l3ef&pwd=5jvm",
+        "platform": "Android",
+        "version": "v1.0",
+        "size": "4.84 MB",
+        "mode": "auto",
+        "filename": "动态菜单生成器_1.0.apk"
+      }
+    },
+    {
+      "name": "T显编译器",
+      "desc": "可快捷编辑 titleraw/tellraw 同时也可拆分纠错 titleraw/tellraw 便于修改。",
+      "icon": "https://s41.ax1x.com/2026/06/20/pmG0SFP.jpg",
+      "badge": "official",
+      "badgeText": "正式版",
+      "download": {
+        "enabled": true,
+        "url": "https://lz.qaiu.top/parser?url=https://share.feijipan.com/s/Se5CQIy4",
+        "platform": "Android",
+        "version": "v1.2",
+        "size": "23.86 MB",
+        "mode": "auto",
+        "filename": "T显编译器_1.2.apk"
+      }
+    },
+    {
+      "name": "选区助手",
+      "desc": "输入x,y,z一键计算dx,dy,dz，便捷添加目标选择器",
+      "icon": "https://s41.ax1x.com/2026/03/21/penvh6g.png",
+      "badge": "official",
+      "badgeText": "正式版",
+      "download": {
+        "enabled": true,
+        "url": "https://lz.qaiu.top/parser?url=https://share.feijipan.com/s/sM8aLctV",
+        "platform": "Android",
+        "version": "v1.0",
+        "size": "3.66 MB",
+        "mode": "auto",
+        "filename": "选区助手_1.0.apk"
+      }
+    },
+    {
+      "name": "命令树",
+      "desc": "可视化编辑命令，便捷操作",
+      "icon": null,
+      "badge": "unreleased",
+      "badgeText": "未发布",
+      "download": {
+        "enabled": false,
+        "platform": "Android",
+        "version": "敬请期待",
+        "size": "-- MB"
+      }
+    }
+  ],
+  "jumpCards": [
+    {
+      "name": "MC指令音乐站",
+      "desc": "提供丰富的Minecraft指令音乐资源下载，支持多种格式，每日更新精选歌单。",
+      "icon": "music",
+      "tag": "v1.3",
+      "url": "https://mcnote.cc.cd",
+      "enabled": true
+    },
+    {
+      "name": "MC头像制作",
+      "desc": "上传照片，添加MC风格装饰图标，自由调整位置、大小、旋转和透明度，一键导出专属头像。",
+      "icon": "user-astronaut",
+      "tag": "v1.1",
+      "url": "https://pic-1bj.pages.dev",
+      "enabled": true
+    },
+    {
+      "name": "网站开源",
+      "desc": "访问网站的开源项目仓库",
+      "icon": "fab fa-github",
+      "tag": "GITHUB",
+      "url": "https://github.com/mc-str/jzfkwz",
+      "enabled": true
+    }
+  ],
+  "disclaimer": {
+    "title": "软件用户许可及免责声明",
+    "body": "下载或使用软件即视为同意本声明。请仔细阅读条款内容，了解软件使用规范及相关责任界定。",
+    "fileUrl": "http://lz.qaiu.top/parser?url=https://share.feijipan.com/s/cV5CW5MK",
+    "fileSize": "9 KB",
+    "requireAcceptBeforeDownload": true,
+    "acceptRememberDays": 30
+  },
+  "contacts": [
+    {
+      "name": "交流群",
+      "url": "https://qun.qq.com/universal-share/share?ac=1&authKey=0Fr6H805IgI3EAI4NnsxS3V%2B8UDXsUbhJ0JDTkNWHA%2FdZyG74QCyGKXTKPTkqFBT",
+      "icon": "users"
+    },
+    {
+      "name": "联系开发者",
+      "url": "https://qm.qq.com/q/sI7UfqUJFu",
+      "icon": "user-tie"
+    }
+  ],
+  "icp": "萌ICP备20268983号",
+  "icpLink": "https://icp.gov.moe/?keyword=20268983",
+  "copyright": "© 2026 #MCCBCOOM73"
+};
